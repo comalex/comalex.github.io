28d4163b1faacdd57ef366c5de67c4f5ed0d8526
alex <stupak.alexn@gmail.com>
2015-01-11 17:47:15 +0200

    fix blog link

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var offset = 80;
 
 $('.navbar li a').click(function(event) {
     event.preventDefault();
+    if ($(this).attr('target') == '_blank')
+         window.open($(this).attr('href'), '_blank');
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
 });
